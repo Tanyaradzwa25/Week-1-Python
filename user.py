0acbc96727fa2caa3ee3ce5c77d6d03ba5ff0d3b
Tanyaradzwa Maponga <samanthamaponga62@gmail.com>
2024-08-21 11:26:35 +0200

    Answers to week 1.py

---
+++ user.py
@@ -0,0 +1,9 @@
+
+name = "Samantha Tanyaradzwa Maponga"
+email = "samanthamaponga62@gmail.com"
+age = 30
+location = "Germiston,South Africa"
+profession = "Accountant and Nurse"
+work = "Life HealthCare"
+
+print(f"Hi my name is {name},l am {age} years old.I am an {profession} and l work at {work} in Primrose.I stay with my sister in {location}. You can reach me on my {email}.")
